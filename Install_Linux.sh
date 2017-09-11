5f00dbba750f0b3ebefee29ec2973fafe79ac5b3
David Dos Santos <revo.dos.santos@gmail.com>
2017-09-11 11:13:15 +1000

    Fixed gitignore

--- Install_Linux.sh
+++ Install_Linux.sh
@@ -3,11 +3,11 @@
 YELLOW='\033[1;33m'
 NC='\033[0m' # No color
 echo -e "${YELLOW}~~~ INSTALLING COMPONENT DEPENDENCIES FOR HIGH-PERFORMANCE-CONTROL ~~~ ${NC}"
-#. Install_Files/Install_c3.sh
+. Install_Files/Install_c3.sh
 . Install_Files/Install_cdyn.sh
-#. Install_Files/Install_c3sc.sh
+. Install_Files/Install_c3sc.sh
 
 echo -e "${YELLOW}~~~ INSTALLING HIGH-PERFORMANCE-CONTROL ~~~ ${NC}"
-#. Install_Files/Install_hpc.sh
+. Install_Files/Install_hpc.sh
 
 echo -e "${YELLOW}~~~ FINISHED ~~~ ${NC}"
