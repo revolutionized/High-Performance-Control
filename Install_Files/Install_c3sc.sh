8d7af428f54260ea41462c8801054571f0d7f9a1
revolutionized <revo.dos.santos@gmail.com>
2017-09-14 08:44:08 +1000

    Fixed linux install problem with a replacement cmake file

--- Install_Files/Install_c3sc.sh
+++ Install_Files/Install_c3sc.sh
@@ -1,11 +1,19 @@
+# Assumes it is in the 
 RED='\033[1;31m'
 NC='\033[0m' # No color
 echo -e " ${RED}-------------------- Installing C3SC Library ${NC}"
-git clone https://github.com/goroda/c3sc.git c3sc
+#git clone https://github.com/goroda/c3sc.git c3sc
 cd c3sc
-mkdir build
+#mkdir build
 cd build
-cmake ..
+#cmake ..
+
+# There is a case in linux where it c3sc can't find the cdyn library, so I've created another CMakeLists to replace it
+# with.
+case "$OSTYPE" in
+  linux*) . ../../Install_Files/Linux_replacement.sh;;
+esac
+
 make
 make install
 cd ../..
---
+++ Install_Files/Linux_replacement.sh
@@ -0,0 +1,3 @@
+# Assumed to be in c3sc/build directory
+mv ../src/CMakeLists.txt ../src/CMakeLists.txt.old
+cp ../../Install_Files/CMake_ReplaceFile ../src/CMakeLists.txt
